afe1431e7c7ebe2d720dbf43b1863c49cb6ea78b
Gustavo Borges <157196538+gsnoopy@users.noreply.github.com>
2026-03-13 11:26:51 -0300

    Create 13 - Palindrome 🏎️.py

---
+++ 2026/03/13 - Palindrome 🏎️.py
@@ -0,0 +1,8 @@
+def check_palindrome(sequence):
+    
+  sequence = sequence.replace(" ", "")
+  inverted = sequence[::-1]
+  
+  if sequence.lower() == inverted.lower():
+  else:
+     return False
